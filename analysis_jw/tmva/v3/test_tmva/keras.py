dc60ab4958d0a7fae1f11a8a5966ba91154790e3
Jiwon Park <minerva1993@gmail.com>
2017-09-10 20:23:22 +0900

    auc = 0.912

--- analysis_jw/tmva/v3/test_tmva/keras.py
+++ analysis_jw/tmva/v3/test_tmva/keras.py
@@ -3,10 +3,11 @@ import sys, os
 import google.protobuf
 from ROOT import *
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
+from keras.layers.core import Dense, Activation, Dropout
 from keras.regularizers import l2
 from keras import initializations
 from keras.optimizers import SGD
+from keras.optimizers import adam
 
 TMVA.Tools.Instance()
 TMVA.PyMethodBase.PyInitialize()
@@ -16,7 +17,7 @@ fout = TFile("output_keras.root","recreate")
 factory = TMVA.Factory("TMVAClassification", fout,
                        "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G;D:AnalysisType=Classification" )
 
-loader = TMVA.DataLoader("keras3")
+loader = TMVA.DataLoader("keras4")
 loader.AddVariable("njets", "I")
 loader.AddVariable("nbjets_m",'I')
 loader.AddVariable("ncjets_m",'I')
@@ -32,10 +33,30 @@ loader.AddVariable("jet1pt",'F')
 loader.AddVariable("jet2pt",'F')
 loader.AddVariable("jet3pt",'F')
 loader.AddVariable("jet4pt",'F')
+loader.AddVariable("jet1eta",'F')
+loader.AddVariable("jet2eta",'F')
+loader.AddVariable("jet3eta",'F')
+loader.AddVariable("jet4eta",'F')
+#loader.AddVariable("jet1phi",'F')
+#loader.AddVariable("jet2phi",'F')
+#loader.AddVariable("jet3phi",'F')
+#loader.AddVariable("jet4phi",'F')
+loader.AddVariable("jet1m",'F')
+loader.AddVariable("jet2m",'F')
+loader.AddVariable("jet3m",'F')
+loader.AddVariable("jet4m",'F')
 loader.AddVariable("jet1csv",'F')
 loader.AddVariable("jet2csv",'F')
 loader.AddVariable("jet3csv",'F')
 loader.AddVariable("jet4csv",'F')
+loader.AddVariable("jet1cvsl",'F')
+loader.AddVariable("jet2cvsl",'F')
+loader.AddVariable("jet3cvsl",'F')
+loader.AddVariable("jet4cvsl",'F')
+loader.AddVariable("jet1cvsb",'F')
+loader.AddVariable("jet2cvsb",'F')
+loader.AddVariable("jet3cvsb",'F')
+loader.AddVariable("jet4cvsb",'F')
 #loader.AddVariable("DRlepWeta",'F')
 #loader.AddVariable("DRlepWphi",'F')
 #loader.AddVariable("DRlepWm",'F')
@@ -103,8 +124,10 @@ loader.AddSignalTree(tsig, 0.156137331574)
 loader.AddBackgroundTree(tbkg1, 0.0910581123792)
 loader.AddBackgroundTree(tbkg2, 0.0910581123792)
 
-sigCut = TCut("njets > 0 && nbjets_m > 0 && missingET > 0 && bjetmDR < 5 && bjetmDEta < 5 && bjetmDPhi < 5 && dibjetsMass < 9999 && bjetPt_dibjetsm < 9999 && cjetPt < 9999 && jet1pt < 9999 && jet2pt < 9999 && jet3pt < 9999 && jet4pt < 9999 && jet1csv > 0 && jet1csv < 1.01 && jet2csv > 0 && jet2csv < 1.01 && jet3csv > 0 && jet3csv < 1.01 && jet4csv > 0 && jet4csv < 1.01 && DRlepWpt > 0 &&DRlepWm > 0")
-bkgCut = TCut("njets > 0 && nbjets_m > 0 && missingET > 0 && bjetmDR < 5 && bjetmDEta < 5 && bjetmDPhi < 5 && dibjetsMass < 9999 && bjetPt_dibjetsm < 9999 && cjetPt < 9999 && jet1pt < 9999 && jet2pt < 9999 && jet3pt < 9999 && jet4pt < 9999 && jet1csv > 0 && jet1csv < 1.01 && jet2csv > 0 && jet2csv < 1.01 && jet3csv > 0 && jet3csv < 1.01 && jet4csv > 0 && jet4csv < 1.01 && DRlepWpt > 0 &&DRlepWm > 0")
+sigCut = TCut("njets > 0 && nbjets_m > 0 && missingET > 0 && bjetmDR < 5 && bjetmDEta < 5 && bjetmDPhi < 5 && dibjetsMass < 9999 && bjetPt_dibjetsm < 9999 && cjetPt < 9999 && jet1pt < 9999 && jet2pt < 9999 && jet3pt < 9999 && jet4pt < 9999 && jet1eta < 99 && jet2eta < 99 && jet3eta < 99 && jet4eta < 99 && jet1m < 999 && jet2m < 999 && jet3m < 999 && jet4m < 999 && jet1csv > 0 && jet1csv < 1.01 && jet2csv > 0 && jet2csv < 1.01 && jet3csv > 0 && jet3csv < 1.01 && jet4csv > 0 && jet4csv < 1.01 && jet1cvsl < 99 && jet2cvsl < 99 && jet3cvsl < 99 && jet4cvsl < 99 && jet1cvsb < 99 && jet2cvsb < 99 && jet3cvsb < 99 && jet4cvsb < 99 && DRlepWpt > 0 &&DRlepWm > 0")
+
+bkgCut = TCut("njets > 0 && nbjets_m > 0 && missingET > 0 && bjetmDR < 5 && bjetmDEta < 5 && bjetmDPhi < 5 && dibjetsMass < 9999 && bjetPt_dibjetsm < 9999 && cjetPt < 9999 && jet1pt < 9999 && jet2pt < 9999 && jet3pt < 9999 && jet4pt < 9999 && jet1eta < 99 && jet2eta < 99 && jet3eta < 99 && jet4eta < 99 && jet1m < 999 && jet2m < 999 && jet3m < 999 && jet4m < 999 && jet1csv > 0 && jet1csv < 1.01 && jet2csv > 0 && jet2csv < 1.01 && jet3csv > 0 && jet3csv < 1.01 && jet4csv > 0 && jet4csv < 1.01 && jet1cvsl < 99 && jet2cvsl < 99 && jet3cvsl < 99 && jet4cvsl < 99 && jet1cvsb < 99 && jet2cvsb < 99 && jet3cvsb < 99 && jet4cvsb < 99 && DRlepWpt > 0 &&DRlepWm > 0")
+
 loader.PrepareTrainingAndTestTree(
     sigCut, bkgCut,
     "nTrain_Signal=30000:nTrain_Background=30000:nTest_Signal=10000:nTest_Background=10000:SplitMode=Random:NormMode=NumEvents:!V"
@@ -119,25 +142,26 @@ def normal(shape, name=None):
   return initializations.normal(shape, scale=0.05, name=name)
 
 model = Sequential()
-model.add(Dense(300, init=normal, activation='relu', W_regularizer=l2(1e-5), input_dim=59))
-model.add(Dense(500, init=normal, activation='relu'))
-model.add(Dense(700, init=normal, activation='relu'))
-model.add(Dense(700, init=normal, activation='relu'))
-model.add(Dense(700, init=normal, activation='relu'))
-model.add(Dense(700, init=normal, activation='relu'))
-model.add(Dense(700, init=normal, activation='relu'))
-model.add(Dense(700, init=normal, activation='relu'))
-model.add(Dense(500, init=normal, activation='relu'))
-model.add(Dense(500, init=normal, activation='relu'))
-model.add(Dense(300, init=normal, activation='relu'))
-model.add(Dense(100, init=normal, activation='relu'))
-model.add(Dense(2, init=normal, activation='softmax'))
+model.add(Dense(300, init='glorot_uniform', activation='relu', W_regularizer=l2(1e-5), input_dim=75))
+model.add(Dense(500, init='glorot_uniform', activation='relu'))
+model.add(Dense(700, init='glorot_uniform', activation='relu'))
+model.add(Dense(700, init='glorot_uniform', activation='relu'))
+model.add(Dense(700, init='glorot_uniform', activation='relu'))
+model.add(Dense(700, init='glorot_uniform', activation='relu'))
+model.add(Dense(700, init='glorot_uniform', activation='relu'))
+model.add(Dense(700, init='glorot_uniform', activation='relu'))
+model.add(Dense(500, init='glorot_uniform', activation='relu'))
+model.add(Dense(500, init='glorot_uniform', activation='relu'))
+model.add(Dense(300, init='glorot_uniform', activation='relu'))
+model.add(Dense(100, init='glorot_uniform', activation='relu'))
+model.add(Dense(2, init='glorot_uniform', activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01), metrics=['accuracy',])
+#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy',])
 model.save('model.h5')
 model.summary()
 
-factory.BookMethod(loader, TMVA.Types.kPyKeras, 'PyKeras',"H:!V:VarTransform=D,G:FilenameModel=model.h5:NumEpochs=20:BatchSize=32")
+factory.BookMethod(loader, TMVA.Types.kPyKeras, 'PyKeras',"H:!V:VarTransform=D,G:FilenameModel=model.h5:NumEpochs=30:BatchSize=100")
 
 factory.TrainAllMethods()
 factory.TestAllMethods()
@@ -145,6 +169,4 @@ factory.EvaluateAllMethods()
 fout.Close()
 
 TMVA.TMVAGui("output_keras.root")
-
-
 
